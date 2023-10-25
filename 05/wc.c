9815b2345464c043e7a742a85946bc3576e6a9f3
libor <libor@local>
2023-10-25 13:14:06 +0200

    added wordcount exercise

--- 05/wc.c
+++ 05/wc.c
@@ -1,28 +1,20 @@
 #include <stdio.h>
-#include <stdbool.h>
 
 int main (int argc, char* argv[]) {
   FILE *file;
   char** filename;
-  bool nonterminatedlastline;
-  int nchars = 0, nlines = 0;
-  char prevchar = EOF, c;
-  if (argc != 2) { fprintf(stderr, "%s filepath\n", *argv); return 1; }
+  /*  další deklarace  */
+  
+  if (argc != 2) { /*  ošetření počtu argumentů  */ }
   filename = &argv[1];
-  file = fopen(*filename, "r");
-  if (file == NULL)
-    { fprintf(stderr, "File %s is inaccessible\n", *argv); return 2; }
-  for (;;) {
-    c = fgetc(file);
-    if (c == EOF) break;
-    nchars++;
-    if (c == '\n') nlines++;
-    prevchar = c;
-  }
-  nonterminatedlastline = prevchar != EOF && prevchar != '\n';
-  if (nonterminatedlastline) nlines++;
-  printf("%s: %d lines, %d characters\n", *filename, nlines, nchars);
-  if (nonterminatedlastline) printf("The last line is not terminated by \\n\n");
-  fclose(file);
+
+  /*  otevření souboru pro čtení  */
+
+  /*  načítání znaků až do EOF a zvyšování čítačů  */
+
+  /*  výpis počtu znaků a řádků  */
+
+  /*  uzavření souboru  */
+
   return 0;
 }
