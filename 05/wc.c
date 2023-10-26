3f2b134d26ecc240b1ba02f631b81f654e9c2cbb
libor <libor@local>
2023-10-26 17:38:53 +0200

    wc exercise: dropped one level of referencing filename

--- 05/wc.c
+++ 05/wc.c
@@ -1,20 +1,19 @@
 #include <stdio.h>
 
 int main (int argc, char* argv[]) {
-  FILE *file;
-  char** filename;
+  FILE *file;  char* filename;
   /*  další deklarace  */
-  
-  if (argc != 2) { /*  ošetření počtu argumentů  */ }
-  filename = &argv[1];
 
-  /*  otevření souboru pro čtení  */
+  if (argc != 2) { /*  ošetření chyby  */ }
+  filename = argv[1];
+
+  /*  otevření souboru file pro čtení + ošetření chyby  */
 
   /*  načítání znaků až do EOF a zvyšování čítačů  */
 
   /*  výpis počtu znaků a řádků  */
 
-  /*  uzavření souboru  */
+  /*  uzavření souboru file  */
 
   return 0;
 }
