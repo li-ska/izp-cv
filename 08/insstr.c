fffd5c7cbead08e3b413bf574fc62aa7523d56ad
libor <libor@local>
2023-11-22 19:22:30 +0100

    added: 8th labs

---
+++ 08/insstr.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// #define DEBUG 1
+#ifdef DEBUG
+#define pmsg(s,...) fprintf(stderr,__FILE__":%u: " s "\n",__LINE__,__VA_ARGS__)
+#else
+#define pmsg(...) {}
+#endif
+
+int insertStr (char *si, char *st, int k) {
+  int li = strlen(si);
+  int lt = strlen(st);
+  if (li == 0) return 0;     // hotovo, prázdná akce
+  if (k<0 || k>lt) return 2; // chybná pozice
+
+     /*  DOPLŇTE  */
+
+  return 0;                  // hotovo
+}
+
+int main (void) {
+  int e;
+  char *st = (char *) malloc(5);
+  if (st == NULL) return 1;
+  strcpy(st, "abcd");
+
+  printf("%s\n", st);
+  e = insertStr("XY", st, 1);
+  if (e == 0) printf("%s\n", st);
+  else fprintf(stderr, "wrong position\n");
+
+  return e;
+}
